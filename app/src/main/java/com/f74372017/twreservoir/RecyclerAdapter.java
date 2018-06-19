a169de7791f1d122fd819f473c3d52a3309f9069
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-06-19 21:53:05 +0800

    v1.0.6 修改目標 SDK 版本26

--- app/src/main/java/com/f74372017/twreservoir/RecyclerAdapter.java
+++ app/src/main/java/com/f74372017/twreservoir/RecyclerAdapter.java
@@ -67,7 +67,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
                 viewHolder.mWaveLoadingView.setCenterTitle(df.format(Double.parseDouble(list.get(i).getPercentage())));
                 viewHolder.mWaveLoadingView.setProgressValue((int)Double.parseDouble(list.get(i).getPercentage()));
                 viewHolder.tvName.setText(list.get(i).getName());
-                viewHolder.tvWater.setText("有效蓄水量:"+list.get(i).getWater()+"立方公頃");
+                viewHolder.tvWater.setText("有效蓄水量:"+list.get(i).getWater()+"立方公尺");
                 viewHolder.tvDay.setText("預測剩餘天數:"+"60天以上");
                 viewHolder.tvUpdate.setText("最後更新日期:"+list.get(i).getUpdate());
                 viewHolder.tvDown.setText("今日進水量:"+list.get(i).getDown());
@@ -80,6 +80,10 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
                     viewHolder.mWaveLoadingView.setCenterTitleStrokeColor(0X9eff3b76);
                    viewHolder.mWaveLoadingView.setBorderColor(0x9eff3b76);
 
+                }
+                // 當有效蓄水為0時
+                if(Float.parseFloat(list.get(i).getPercentage())==0){
+                    viewHolder.tvDay.setText("預測剩餘天數:"+"0天");
                 }
     }
 
